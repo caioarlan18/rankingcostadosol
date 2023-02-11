7601f08a1651c4a901d184f44c9a28129aa65b7f
Caio Arlan Gomes <108774432+caioarlan18@users.noreply.github.com>
2023-02-11 00:10:23 -0300

    general updates

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var menu = document.querySelector('nav.nv')
 var sec = document.querySelector('section.sec')
 var chave = document.querySelector('section.chav')
 
-
+// menu hamburguer
 function menu_hamburguer() {
     menu.classList.toggle('open')
 }
 
+
+// adaptação single page sem framework
 function close() {
     menu.classList.remove('open')
 }
@@ -26,4 +28,23 @@ function chav() {
     chave.classList.add('chaveopen')
     menu.classList.remove("open")
     sec.classList.remove('reg')
+}
+
+// menu arrow(submenu home mobile e desktop)
+function arrow() {
+    // mobile
+    var submenu1 = document.querySelector("a.submenu1")
+    var submenu2 = document.querySelector("a.submenu2")
+
+    // desktop
+    var submenu3 = document.querySelector("a.submenu3")
+    var submenu4 = document.querySelector("a.submenu4")
+
+    // mobile
+    submenu1.classList.toggle("submenuopen")
+    submenu2.classList.toggle('submenuopen')
+
+    // desktop
+    submenu3.classList.toggle("submenuopen1")
+    submenu4.classList.toggle('submenuopen1')
 }
