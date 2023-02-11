bdf62edd05288d1acae0c17876e7cffee72b7174
Caio Arlan Gomes <108774432+caioarlan18@users.noreply.github.com>
2023-02-10 21:00:01 -0300

    update de single page

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 var menu = document.querySelector('nav.nv')
+var sec = document.querySelector('section.sec')
+var chave = document.querySelector('section.chav')
+
+
 function menu_hamburguer() {
     menu.classList.toggle('open')
 }
 
 function close() {
     menu.classList.remove('open')
+}
+
+function reg() {
+    sec.classList.add('reg')
+    menu.classList.remove("open")
+    chave.classList.remove('chaveopen')
+}
+
+function home() {
+    sec.classList.remove("reg")
+    chave.classList.remove('chaveopen')
+    menu.classList.remove("open")
+}
+function chav() {
+    chave.classList.add('chaveopen')
+    menu.classList.remove("open")
+    sec.classList.remove('reg')
 }
