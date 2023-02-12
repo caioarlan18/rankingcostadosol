9caa41965e43bf371e762b3f5e51143109f3b07c
Caio Arlan Gomes <108774432+caioarlan18@users.noreply.github.com>
2023-02-12 17:46:56 -0300

    melhorias na parte tecnica e remoção de bugs

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var back = document.querySelector("div.back")
 var orgpage = document.querySelector("section.orgpage")
 var tabela = document.querySelector('table.tabela')
 var tabela2 = document.querySelector("section.tabela2")
+var tabela1 = document.querySelector('table.tabela1')
 // menu hamburguer
 function menu_hamburguer() {
     menu.classList.toggle('open')
@@ -26,6 +27,7 @@ function reg() {
     orgpage.classList.remove("orgpageopen")
     tabela.classList.add('htabela')
     tabela2.classList.add('htabela')
+    tabela1.classList.add('htabela')
 }
 
 function home() {
@@ -37,6 +39,7 @@ function home() {
     orgpage.classList.remove("orgpageopen")
     tabela.classList.remove('htabela')
     tabela2.classList.remove('htabela')
+    tabela1.classList.remove('htabela')
 }
 function chav() {
     chave.classList.add('chaveopen')
@@ -47,6 +50,7 @@ function chav() {
     orgpage.classList.remove("orgpageopen")
     tabela.classList.add('htabela')
     tabela2.classList.add('htabela')
+    tabela1.classList.add('htabela')
 }
 
 // menu arrow(submenu home mobile e desktop)
@@ -71,21 +75,17 @@ function arrow() {
 
 // pagina org.html tabela de pontuação
 
-// Variáveis para as tabelas
+// pontuação categoria A
+
 var tabelaEditavel = document.querySelector('table#tabelaEditavel');
 var tabelaNaoEditavel = document.querySelector('table#tabelaNaoEditavel');
-
-// Adiciona event listener a cada célula da tabela editável
 tabelaEditavel.addEventListener('input', (e) => {
     if (e.target.tagName === 'TD') {
-        // Encontra a célula correspondente na tabela não editável e atualiza seu conteúdo
         var linha = e.target.parentNode;
         var celulaNaoEditavel = tabelaNaoEditavel.rows[linha.rowIndex].cells[e.target.cellIndex];
         celulaNaoEditavel.textContent = e.target.textContent;
     }
 });
-
-// Adiciona um botão para adicionar linhas dinamicamente
 var addnew = document.querySelector("button.addnew");
 addnew.addEventListener('click', () => {
     var trEditavel = document.createElement("tr");
@@ -97,7 +97,7 @@ addnew.addEventListener('click', () => {
     trEditavel.appendChild(td2Editavel);
     tabelaEditavel.appendChild(trEditavel);
 
-    // Adiciona uma nova linha à tabela não editável com o mesmo conteúdo
+
     var trNaoEditavel = document.createElement("tr");
     var td1NaoEditavel = document.createElement('td');
     var td2NaoEditavel = document.createElement("td");
@@ -106,9 +106,8 @@ addnew.addEventListener('click', () => {
     trNaoEditavel.appendChild(td1NaoEditavel);
     trNaoEditavel.appendChild(td2NaoEditavel);
     tabelaNaoEditavel.appendChild(trNaoEditavel);
-    // Adiciona o botão de remoção
-    var btnRemover = document.createElement("button");
 
+    var btnRemover = document.createElement("button");
     btnRemover.addEventListener('click', () => {
         trEditavel.remove();
         trNaoEditavel.remove();
@@ -119,9 +118,51 @@ addnew.addEventListener('click', () => {
 });
 
 
+// pontuaçao categoria B
+var tabelaEditavel2 = document.querySelector('table#tabelaEditavel2');
+var tabelaNaoEditavel2 = document.querySelector('table#tabelaNaoEditavel2');
+tabelaEditavel2.addEventListener('input', (e) => {
+    if (e.target.tagName === 'TD') {
+        var linha = e.target.parentNode;
+        var celulaNaoEditavel2 = tabelaNaoEditavel2.rows[linha.rowIndex].cells[e.target.cellIndex];
+        celulaNaoEditavel2.textContent = e.target.textContent;
+    }
+});
+var addnewb = document.querySelector("button.addnewb");
+addnewb.addEventListener('click', () => {
+    var trEditavel2 = document.createElement("tr");
+    var td1Editavel2 = document.createElement('td');
+    var td2Editavel2 = document.createElement("td");
+    td1Editavel2.contentEditable = true;
+    td2Editavel2.contentEditable = true;
+    trEditavel2.appendChild(td1Editavel2);
+    trEditavel2.appendChild(td2Editavel2);
+    tabelaEditavel2.appendChild(trEditavel2);
 
 
-// Função que cria uma nova linha na tabela editável e na tabela não editável
+    var trNaoEditavel2 = document.createElement("tr");
+    var td1NaoEditavel2 = document.createElement('td');
+    var td2NaoEditavel2 = document.createElement("td");
+    td1NaoEditavel2.textContent = td1Editavel2.textContent;
+    td2NaoEditavel2.textContent = td2Editavel2.textContent;
+    trNaoEditavel2.appendChild(td1NaoEditavel2);
+    trNaoEditavel2.appendChild(td2NaoEditavel2);
+    tabelaNaoEditavel2.appendChild(trNaoEditavel2);
+
+    var btnRemover = document.createElement("button");
+    btnRemover.addEventListener('click', () => {
+        trEditavel2.remove();
+        trNaoEditavel2.remove();
+    });
+    btnRemover.classList.add('remove')
+    trEditavel2.appendChild(btnRemover);
+
+});
+
+
+
+// chave categoria A
+
 function criarNovaLinha() {
     const select = document.querySelector("select#option");
     const chavEditavel = document.querySelector('table#chavEditavel');
@@ -152,12 +193,44 @@ function criarNovaLinha() {
     chavEditavel.appendChild(br)
     tabelaNaoEditavel.appendChild(broken)
 }
-
-
-// Event listener para o botão "Adicionar"
 const addnew2 = document.querySelector('button.addnew2');
 addnew2.addEventListener('click', criarNovaLinha);
 
+
+
+// chave categoria B
+function criarNovaLinha1() {
+    const select = document.querySelector("select#option1");
+    const chavEditavel = document.querySelector('table#chavEditavel2');
+    const tabelaNaoEditavel = document.querySelector("table#chavNaoEditavel2");
+    const tr = document.createElement('tr');
+    const trNaoEditavel = document.createElement('tr');
+    const br = document.createElement('br')
+    const broken = document.createElement('br')
+    for (let i = 0; i < select.value; i++) {
+        const td = document.createElement('td');
+        td.contentEditable = true;
+        td.addEventListener('input', (event) => {
+            const index = Array.from(tr.children).indexOf(event.target);
+            trNaoEditavel.children[index].textContent = event.target.textContent;
+        });
+
+        tr.appendChild(td);
+
+
+        const tdNaoEditavel = document.createElement('td');
+        tdNaoEditavel.textContent = td.textContent;
+        trNaoEditavel.appendChild(tdNaoEditavel);
+    }
+
+    chavEditavel.appendChild(tr);
+    tabelaNaoEditavel.appendChild(trNaoEditavel);
+    chavEditavel.appendChild(br)
+    chavEditavel.appendChild(br)
+    tabelaNaoEditavel.appendChild(broken)
+}
+const addnew3 = document.querySelector('button.addnew3');
+addnew3.addEventListener('click', criarNovaLinha1);
 
 
 // pagina do org
@@ -170,6 +243,7 @@ function org() {
     orgpage.classList.remove("orgpageopen")
     tabela.classList.add('htabela')
     tabela2.classList.add('htabela')
+    tabela1.classList.add('htabela')
 }
 
 // login page
@@ -187,8 +261,12 @@ var entrar = document.querySelector('input.entrar').addEventListener('click', ()
     }
     else if (email.value != '1') {
         alert('Email ou senha incorretos')
+        email.value = ''
+        senha.value = ''
     } else if (senha.value != '1') {
         alert('Email ou senha incorretos')
+        email.value = ''
+        senha.value = ''
     }
     else {
         orgpage.classList.add("orgpageopen")
